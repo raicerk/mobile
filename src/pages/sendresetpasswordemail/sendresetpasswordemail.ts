1e81a3fb049188bfcc7e1c3a38e3afa27c09f752
Juan Mora <raicerk@gmail.com>
2018-08-12 17:01:23 -0300

    Add page sendresetpasswordemail

---
+++ src/pages/sendresetpasswordemail/sendresetpasswordemail.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the SendresetpasswordemailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-sendresetpasswordemail',
+  templateUrl: 'sendresetpasswordemail.html',
+})
+export class SendresetpasswordemailPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SendresetpasswordemailPage');
+  }
+
+}
