177afd7415990df5a5bb9a968358a9af90608983
Juan Mora <raicerk@gmail.com>
2018-08-11 23:59:19 -0400

    Add provider rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import GoNFetch from 'go-n-fetch';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  gnf: any;
+
+  constructor() {
+    this.gnf = GoNFetch({
+      baseUrl: 'https://mobile.raregenomics.org/api/'
+    })
+  }
+
+  post(url: string, data: any) {
+    return new Promise(function (resolve, reject) {
+      this.gnf.post(url, {
+        body: JSON.stringify(data)
+      }).then(response => {
+        resolve(response);
+      }).catch(error => {
+        reject(error);
+      });
+    })
+
+  }
+
+  get(url: string) {
+    return new Promise(function (resolve, reject) {
+      this.gnf.get(url).then(response => {
+        resolve(response);
+      }).catch(error => {
+        reject(error);
+      })
+    })
+  }
+
+}
