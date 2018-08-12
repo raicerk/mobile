be606e745643b2745dea9dd3b55a24fc18397569
Juan Mora <raicerk@gmail.com>
2018-08-11 23:19:17 -0400

    Add function and view of Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -17,8 +17,45 @@ export class LoginPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  signIn() {
+
+    /* let body = this.jsonToURLEncoded({
+      email: this.localdata['email'],
+      password: this.localdata['password']
+    });
+    var link = 'https://mobile.raregenomics.org/api/signin';
+    let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' });
+    let options = new RequestOptions({
+      headers: headers
+    });
+    this.http.post(link, body, options)
+      .subscribe(data => {
+        if (data.status == 200) {
+          console.log("Login OK");
+          this.saveData();
+        } else if (data.status == 202) {
+          let alert = this.alertCtrl.create({
+            title: 'Message',
+            subTitle: data.json().message,
+            buttons: ['OK']
+          });
+          alert.present();
+        } else if (data.status == 400) {
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: data.json().message,
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+      }, error => {
+        console.log(JSON.stringify(error.json()));
+      }); */
+  }
+
+
+  forgotPassword() {
+    //this.navCtrl.push(SendResetPasswordEmailPage);
   }
 
 }
