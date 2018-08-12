27f898fb1970f2e570234a83298a571eacf6c994
Juan Mora <raicerk@gmail.com>
2018-08-12 19:14:05 -0300

    Functionality at sendresetpasswordandemail

--- src/pages/sendresetpasswordemail/sendresetpasswordemail.ts
+++ src/pages/sendresetpasswordemail/sendresetpasswordemail.ts
@@ -14,11 +14,64 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SendresetpasswordemailPage {
 
+  private email;
+  private emailNotFound = true;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SendresetpasswordemailPage');
+  }
+
+  sendResetPasswordLink() {
+    /* if (this.email == undefined) {
+      const alert = this.alertCtrl.create({
+        title: 'Message',
+        subTitle: "Please type your email",
+        buttons: ['OK']
+      });
+
+      alert.present();
+    } else {
+      const url = 'https://mobile.raregenomics.org/api/sendResetPasswordEmail';
+      const headers = new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      });
+      const options = new RequestOptions({
+        headers
+      });
+      const body = "email=" + this.email;
+
+      this.http.post(url, body, options).subscribe(data => {
+        if (data.status == 200) {
+          if (data.json().userExists) {
+            const alert = this.alertCtrl.create({
+              title: 'Message',
+              subTitle: 'Please check your registered email for the reset password link',
+              buttons: ['OK']
+            });
+
+            alert.present();
+            this.navCtrl.pop();
+          } else {
+            this.emailNotFound = false;
+          }
+        }
+      }, error => {
+        const alert = this.alertCtrl.create({
+          title: 'Error',
+          subTitle: error.json().message,
+          buttons: ['OK']
+        });
+
+        alert.present();
+      });
+    } */
+  }
+
+
+  closeWindow() {
+    this.navCtrl.pop();
   }
 
 }
