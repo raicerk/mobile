e107d4b135693f46bef067ccec5d0c176e0da65a
Juan Mora <raicerk@gmail.com>
2018-08-12 19:48:20 -0300

    Email register added

--- src/pages/emailaddress/emailaddress.ts
+++ src/pages/emailaddress/emailaddress.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+
 /**
  * Generated class for the EmailaddressPage page.
  *
@@ -14,11 +16,36 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class EmailaddressPage {
 
+  localdata = {};
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.localdata['email'] = 'res.rows.item(i).email';
+    this.localdata['emailexist'] = true;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EmailaddressPage');
   }
+
+  checkEmail() {   
+    /* let body = this.jsonToURLEncoded({
+      email: this.localdata['email']
+    });
+    var link = 'https://mobile.raregenomics.org/api/checkemail';
+    let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' });
+    let options = new RequestOptions({
+      headers: headers
+    });
+    this.http.post(link, body, options)
+      .subscribe(data => {
+        if (data.status == 201) {          
+          this.saveData();
+        } else if (data.status == 202) { */
+          this.navCtrl.push(LoginPage);
+       /*  }
+      }, error => {
+        console.log(JSON.stringify(error.json()));
+      }); */
+  }
+
 
 }
